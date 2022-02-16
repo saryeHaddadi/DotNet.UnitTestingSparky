be6baaa4aed457c637a8766ee005dc7ba6a76f07
sarye.haddadi <sarye@live.fr>
2022-02-16 17:25:01 +0100

    Start Assignment 2

---
+++ SparkyNUnitTest/GradingCalculatorNUnitTest.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest;
+
+[TestFixture]
+public class GradingCalculatorNUnitTest
+{
+	private GradingCalculator grader { get; set; }
+
+	[SetUp]
+	public void Setup()
+	{
+		grader = new GradingCalculator();
+	}
+
+}
+
