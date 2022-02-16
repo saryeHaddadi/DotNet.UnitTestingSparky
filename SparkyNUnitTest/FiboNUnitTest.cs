a38deec2975d8a9a6b9a70e062b9d3b261e21e06
sarye.haddadi <sarye@live.fr>
2022-02-16 20:22:25 +0100

    Fibo example

---
+++ SparkyNUnitTest/FiboNUnitTest.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest;
+
+[TestFixture]
+public class FiboNUnitTest
+{
+	private Fibo fiboGenerator;
+	[SetUp]
+	public void SetUp()
+	{
+		fiboGenerator = new Fibo();
+	}
+
+	[Test]
+	public void Fibo_InputRange1_ReturnsRightFiboSerie()
+	{
+		// Arrange
+		fiboGenerator.Range = 1;
+
+		// Act
+		var actualResult = fiboGenerator.GetFiboSeries();
+
+		// Assert
+		var expected = new List<int> { 0 };
+		Assert.That(actualResult, Is.Not.Empty);
+		Assert.That(actualResult, Is.Ordered);
+		Assert.That(actualResult, Is.EquivalentTo(expected));
+	}
+
+	[Test]
+	public void Fibo_InputRange6_ReturnsCount6()
+	{
+		// Arrange
+		fiboGenerator.Range = 6;
+
+		// Act
+		var actualResult = fiboGenerator.GetFiboSeries().Count;
+
+		// Assert
+		var expected = 6;
+		Assert.AreEqual(expected, actualResult);
+	}
+
+	[Test]
+	public void Fibo_InputRange6_ReturnsContains3()
+	{
+		// Arrange
+		fiboGenerator.Range = 6;
+
+		// Act
+		var actualResult = fiboGenerator.GetFiboSeries();
+
+		// Assert
+		Assert.That(actualResult, Does.Contain(3));
+	}
+
+	[Test]
+	public void Fibo_InputRange6_ReturnsDontInclude()
+	{
+		// Arrange
+		fiboGenerator.Range = 6;
+
+		// Act
+		var actualResult = fiboGenerator.GetFiboSeries();
+
+		// Assert
+		Assert.That(actualResult, Does.Not.Contain(4));	
+	}
+
+	[Test]
+	public void Fibo_InputRange6_ReturnsRightResult()
+	{
+		// Arrange
+		fiboGenerator.Range = 6;
+
+		// Act
+		var actualResult = fiboGenerator.GetFiboSeries();
+
+		// Assert
+		var expected = new List<int>() { 0, 1, 1, 2, 3, 5 };
+		Assert.AreEqual(expected, actualResult);
+	}
+}
+
--- SparkyNUnitTest/GradingCalculatorNUnitTest.cs
+++ SparkyNUnitTest/GradingCalculatorNUnitTest.cs
@@ -19,5 +19,101 @@ public class GradingCalculatorNUnitTest
 		grader = new GradingCalculator();
 	}
 
+	[Test]
+	public void GetGrade_InputScore95AndAttendance90_ReturnsAGrade()
+	{
+		// Arrange
+		grader.Score = 95;
+		grader.AttendancePercentage = 90;
+
+		// Act
+		var actualResult = grader.GetGrade();
+
+		// Assert
+		var expected = "A";
+		Assert.AreEqual(expected, actualResult);
+	}
+
+	[Test]
+	public void GetGrade_InputScore85AndAttendance90_ReturnsBGrade()
+	{
+		// Arrange
+		grader.Score = 85;
+		grader.AttendancePercentage = 90;
+
+		// Act
+		var actualResult = grader.GetGrade();
+
+		// Assert
+		var expected = "B";
+		Assert.AreEqual(expected, actualResult);
+	}
+
+	[Test]
+	public void GetGrade_InputScore65AndAttendance90_ReturnsCGrade()
+	{
+		// Arrange
+		grader.Score = 65;
+		grader.AttendancePercentage = 90;
+
+		// Act
+		var actualResult = grader.GetGrade();
+
+		// Assert
+		var expected = "C";
+		Assert.AreEqual(expected, actualResult);
+	}
+
+	[Test]
+	public void GetGrade_InputScore95AndAttendance65_ReturnsBGrade()
+	{
+		// Arrange
+		grader.Score = 95;
+		grader.AttendancePercentage = 65;
+
+		// Act
+		var actualResult = grader.GetGrade();
+
+		// Assert
+		var expected = "B";
+		Assert.AreEqual(expected, actualResult);
+	}
+
+	[Test]
+	[TestCase(95, 55)]
+	[TestCase(65, 55)]
+	[TestCase(50, 90)]
+	public void GetGrade_FailureScenarios_ReturnsFGrade(int score, int attendancePercentage)
+	{
+		// Arrange
+		grader.Score = score;
+		grader.AttendancePercentage = attendancePercentage;
+
+		// Act
+		var actualResult = grader.GetGrade();
+
+		// Assert
+		var expected = "F";
+		Assert.AreEqual(expected, actualResult);
+	}
+
+	[Test]
+	[TestCase(95, 90, ExpectedResult = "A")]
+	[TestCase(85, 90, ExpectedResult = "B")]
+	[TestCase(65, 90, ExpectedResult = "C")]
+	[TestCase(95, 65, ExpectedResult = "B")]
+	[TestCase(95, 55, ExpectedResult = "F")]
+	[TestCase(65, 55, ExpectedResult = "F")]
+	[TestCase(50, 90, ExpectedResult = "F")]
+	public string GetGrade_AllScenarios_ReturnsGrade(int score, int attendancePercentage)
+	{
+		// Arrange
+		grader.Score = score;
+		grader.AttendancePercentage = attendancePercentage;
+
+		// Act
+		return grader.GetGrade();
+	}
+
 }
 
